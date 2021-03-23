441fd6477dab456c7e2cb9d816d38ab466a161c4
Mirxoliqov Sayfulla <61648680+MirkhalikovSayfulla@users.noreply.github.com>
2021-03-23 05:12:23 +0000

    Time: 40 ms (95.81%), Space: 14.4 MB (46.77%) - LeetHub

---
+++ two-sum/two-sum.py
@@ -0,0 +1,10 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        pair = dict()
+        i = 0
+        for n in nums:
+            if n in pair:
+                return [pair[n], i]
+            else:
+                pair[target-n] = i
+                i += 1
