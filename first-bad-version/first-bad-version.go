743a185ee493d83c91532ca88db686fb89dca3f9
Mirkhalikov Sayfulla <61648680+MirkhalikovSayfulla@users.noreply.github.com>
2021-04-26 01:34:56 +0500

    Time: 2956 ms (5.08%), Space: 2 MB (14.58%) - LeetHub

---
+++ first-bad-version/first-bad-version.go
@@ -0,0 +1,16 @@
+/** 
+ * Forward declaration of isBadVersion API.
+ * @param   version   your guess about first bad version
+ * @return 	 	      true if current version is bad 
+ *			          false if current version is good
+ * func isBadVersion(version int) bool;
+ */
+
+func firstBadVersion(n int) int {
+    for i := 1; i < n; i++ {
+        if isBadVersion(i) {
+            return i
+        }
+    }
+    return n
+}
